84b20951c5468aeb689009034b1c4b94bab06369
Adam R. Welle <arwelle@cert.org>
2024-10-29 21:14:05 -0400

    version bump

--- version.php
+++ version.php
@@ -46,7 +46,7 @@ DM24-1175
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2024102802;
+$plugin->version = 2024102900;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2024042201;
@@ -56,7 +56,7 @@ $plugin->component = 'mod_topomojo';
 
 // This is a list of plugins, this plugin depends on (and their versions).
 $plugin->dependencies = [
-  'qtype_mojomatch' => 2024091901,
+  'qtype_mojomatch' => 2024102900,
 ];
 
 // This is a stable release.
