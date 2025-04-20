bf816302aa7984f0cf72845ddcfb6fdd80fe8ba2
Nuria Pacheco <npacheco@sei.cmu.edu>
2025-04-20 06:11:53 -0400

    version bump

--- version.php
+++ version.php
@@ -46,7 +46,7 @@ DM24-1175
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2025041100;
+$plugin->version = 2025042000;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2024042201;
