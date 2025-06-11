62ff9f5151036304374953985b8bc81c198df7bd
sei-npacheco <106097162+sei-npacheco@users.noreply.github.com>
2025-06-11 10:40:09 -0400

    Update version.php

--- version.php
+++ version.php
@@ -46,7 +46,7 @@ DM24-1175
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2025053000;
+$plugin->version = 2025060400;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2024042201;
