cmu-sei/moodle-mod_topomojo

commit a4e4b1fa7e247e8ea6836bac2dba41be21108ccc

vm banner, db issue

renderer.php
@@ -67,6 +67,21 @@ class mod_topomojo_renderer extends \plugin_renderer_base {
         echo $this->render_from_template('mod_topomojo/detail', $data);
     }
 
+    public function display_detail_no_vms($topomojo, $duration, $code = false, $showWarning = true) {
+        $data = new stdClass();
+        $data->name = $topomojo->name;
+        $data->intro = $topomojo->intro;
+        $data->code = $code;
+        $data->durationtext = get_string('durationtext', 'mod_topomojo');
+        $data->duration = $duration / 60;
+        
+        // Add the flag to the data object
+        $data->showWarning = $showWarning;
+    
+        echo $this->render_from_template('mod_topomojo/display_no_vms', $data);
+    }
+    
+
     /**
      * Renders the start form for Topomojo activity with Markdown content.
      *

classes/questionmanager.php
@@ -707,7 +707,6 @@ class questionmanager {
                     $this->topomojoquestions[$topomojoqid]->points,
                     $questions[$questionid]
                 );
-                var_dump($topomojoquestion);
                 $qbankorderedquestions[$topomojoqid] = $topomojoquestion; 
                 
             } else {

classes/topomojo_attempt.php
@@ -206,7 +206,6 @@ class topomojo_attempt {
                 $newid = $DB->insert_record('topomojo_attempts', $this->attempt);
                 $this->attempt->id = $newid;
             } catch (\Exception $e) {
-                var_dump($e);
                 return false; // return false on failure
             }
         }

db/upgrade.php
@@ -433,6 +433,23 @@ function xmldb_topomojo_upgrade($oldversion) {
         // Topomojo savepoint reached.
         upgrade_mod_savepoint(true, 2024070304, 'topomojo');
     }
+    if ($oldversion < 2024092201) {
+
+        // Define the table topomojo_questions to update.
+        $table = new xmldb_table('topomojo_questions');
+    
+        // Define the new index for the 'topomojoid' field.
+        $index = new xmldb_index('topomojoid_ix', XMLDB_INDEX_NOTUNIQUE, ['topomojoid']);
+    
+        // Conditionally add the index if it does not exist.
+        if (!$dbman->index_exists($table, $index)) {
+            $dbman->add_index($table, $index);
+        }
+    
+        // Topomojo savepoint reached.
+        upgrade_mod_savepoint(true, 2024092201, 'topomojo');
+    }
+    
 
     return true;
 }

templates/display_no_vms.mustache
@@ -1,0 +1,47 @@
+{{!
+This file is part of Moodle - http://moodle.org/
+
+Moodle is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Moodle is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+Topomojo Plugin for Moodle
+Copyright 2020 Carnegie Mellon University.
+NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+Released under a GNU GPL 3.0-style license, please see license.txt or contact permission@sei.cmu.edu for full terms.
+[DISTRIBUTION STATEMENT A] This material has been approved for public release and unlimited distribution.  Please see Copyright notice for non-US Government use and distribution.
+This Software includes and/or makes use of the following Third-Party Software subject to its own license:
+1. Moodle (https://docs.moodle.org/dev/License) Copyright 1999 Martin Dougiamas.
+DM20-0196
+}}
+
+{{!
+    @template mod_topomojo/detail
+
+    This template renders details about the lab.
+
+            Example context (json):
+            {
+                "name": "",
+                "intro": ""
+            }
+}}
+{{#showWarning}}
+<div class="alert alert-warning">No VMs available for this event. Please contact support.</div>
+{{/showWarning}}
+
+{{#duration}}
+<p>{{durationtext}}: {{duration}} Minutes</p>
+{{/duration}}
+{{#code}}
+<p>{{# str }} supportcode, mod_topomojo {{/ str }}{{code}}</p>
+{{/code}}

view.php
@@ -185,68 +185,88 @@ if ((int)$object->topomojo->grade > 0) {
     $showgrade = false;
 }
 
-//$renderer = $PAGE->get_renderer('mod_topomojo');
 $renderer = $object->renderer;
 echo $renderer->header();
 
 if ($object->event) {
-    $code = substr($object->event->id, 0, 8);
 
-    $renderer->display_detail($topomojo, $topomojo->duration, $code);
+    $object->event->vms = null;
 
-    $jsoptions = ['keepaliveinterval' => 1];
+    if (!isset($object->event->vms) || !is_array($object->event->vms) || empty($object->event->vms)) {
+        // If VMs array is missing or not valid, display error and stop further processing
+        stop_event($object->userauth, $object->event->id);
+        topomojo_end($cm, $context, $topomojo);
+        $markdown = get_markdown($object->userauth, $object->topomojo->workspaceid);
+        $markdowncutline = "/\n---\n/";
+        $parts = preg_split($markdowncutline, $markdown);
+        $renderer->display_detail_no_vms($topomojo, $topomojo->duration);
 
-    $PAGE->requires->js_call_amd('mod_topomojo/keepalive', 'init', [$jsoptions]);
-
-    $extend = false;
-    if ($object->userauth && $topomojo->extendevent) {
-        $extend = true;
-    }
-
-    $renderer->display_controls($starttime, $endtime, $extend, $url, $object->topomojo->workspaceid);
-    // No matter what, start our session timer
-    $PAGE->requires->js_call_amd('mod_topomojo/clock', 'init',
-                                ['starttime' => $starttime, 'endtime' => $endtime, 'id' => $object->event->id]);
-    if ($topomojo->clock == 1) {
-        $PAGE->requires->js_call_amd('mod_topomojo/clock', 'countdown');
-    } else if ($topomojo->clock == 2) {
-        $PAGE->requires->js_call_amd('mod_topomojo/clock', 'countup');
-    }
-
-    $jsoptions = ['id' => $object->event->id, 'topomojo_api_url' => get_config('topomojo', 'topomojoapiurl')];
-    $PAGE->requires->js_call_amd('mod_topomojo/invite', 'init', [$jsoptions]);
-
-    if ($embed == 1) {
-
-        $vmlist = [];
-        if (!is_array($object->event->vms)) {
-            throw new moodle_exception("No VMs visible to user");
+        if ($showgrade) {
+            $renderer->display_grade($topomojo);
         }
-        $jsoptions = ['id' => $object->event->id];
-        $PAGE->requires->js_call_amd('mod_topomojo/ticket', 'init', [$jsoptions]);
 
-        foreach ($object->event->vms as $vm) {
-            if (is_array($vm)) {
-                if ($vm['isVisible']) {
-                    $vmdata['url'] = get_config('topomojo', 'topomojobaseurl') .
-                                     "/mks/?f=1&s=" . $vm['isolationId'] . "&v=" . $vm['name'];
-                    $vmdata['name'] = $vm['name'];
-                    array_push($vmlist, $vmdata);
+        // Display start form
+        $renderer->display_startform($url, $object->topomojo->workspaceid, $parts[0]);
+    } else {
+
+        $code = substr($object->event->id, 0, 8);
+
+        $renderer->display_detail($topomojo, $topomojo->duration, $code);
+
+        $jsoptions = ['keepaliveinterval' => 1];
+
+        $PAGE->requires->js_call_amd('mod_topomojo/keepalive', 'init', [$jsoptions]);
+
+        $extend = false;
+        if ($object->userauth && $topomojo->extendevent) {
+            $extend = true;
+        }
+
+        $renderer->display_controls($starttime, $endtime, $extend, $url, $object->topomojo->workspaceid);
+        // No matter what, start our session timer
+        $PAGE->requires->js_call_amd('mod_topomojo/clock', 'init',
+                                    ['starttime' => $starttime, 'endtime' => $endtime, 'id' => $object->event->id]);
+        if ($topomojo->clock == 1) {
+            $PAGE->requires->js_call_amd('mod_topomojo/clock', 'countdown');
+        } else if ($topomojo->clock == 2) {
+            $PAGE->requires->js_call_amd('mod_topomojo/clock', 'countup');
+        }
+
+        $jsoptions = ['id' => $object->event->id, 'topomojo_api_url' => get_config('topomojo', 'topomojoapiurl')];
+        $PAGE->requires->js_call_amd('mod_topomojo/invite', 'init', [$jsoptions]);
+
+        if ($embed == 1) {
+
+            $vmlist = [];
+            if (!is_array($object->event->vms)) {
+                throw new moodle_exception("No VMs visible to user");
+            }
+            $jsoptions = ['id' => $object->event->id];
+            $PAGE->requires->js_call_amd('mod_topomojo/ticket', 'init', [$jsoptions]);
+
+            foreach ($object->event->vms as $vm) {
+                if (is_array($vm)) {
+                    if ($vm['isVisible']) {
+                        $vmdata['url'] = get_config('topomojo', 'topomojobaseurl') .
+                                        "/mks/?f=1&s=" . $vm['isolationId'] . "&v=" . $vm['name'];
+                        $vmdata['name'] = $vm['name'];
+                        array_push($vmlist, $vmdata);
+                    }
+                } else {
+                    if ($vm->isVisible) {
+                        $vmdata['url'] = get_config('topomojo', 'topomojobaseurl') .
+                                        "/mks/?f=1&s=" . $vm->isolationId . "&v=" . $vm->name;
+                        $vmdata['name'] = $vm->name;
+                        array_push($vmlist, $vmdata);
+                    }
                 }
-            } else {
-                if ($vm->isVisible) {
-                    $vmdata['url'] = get_config('topomojo', 'topomojobaseurl') .
-                                     "/mks/?f=1&s=" . $vm->isolationId . "&v=" . $vm->name;
-                    $vmdata['name'] = $vm->name;
-                    array_push($vmlist, $vmdata);
-                }
+
             }
 
+            $renderer->display_embed_page($object->openAttempt->launchpointurl, $object->event->markdown, $vmlist);
+        } else {
+            $renderer->display_link_page($object->openAttempt->launchpointurl);
         }
-
-        $renderer->display_embed_page($object->openAttempt->launchpointurl, $object->event->markdown, $vmlist);
-    } else {
-        $renderer->display_link_page($object->openAttempt->launchpointurl);
     }
 
 } else {

commit fb89090199a3a10bdf846464d3b8805d0bfa9df6

Fix extend button not displaying
The display_controls() method was not accepting url and workspaceid parameters
that are required by the controls.mustache template for the stop form and
extend button to work properly.

Bug introduced in commit a4e4b1fa (Oct 2024) when display_controls call in
view.php was updated to pass these parameters but the method signature in
renderer.php was not updated to match.

renderer.php
@@ -473,11 +473,13 @@ class mod_topomojo_renderer extends \plugin_renderer_base {
      * @param bool $extend Optional. Whether to include an extension option in the controls.
      * @return void
      */
-    public function display_controls($starttime, $endtime, $extend = false) {
+    public function display_controls($starttime, $endtime, $extend = false, $url = '', $workspaceid = '') {
 
         $data = new stdClass();
         $data->starttime = $starttime;
         $data->endtime = $endtime;
+        $data->url = $url;
+        $data->workspaceid = $workspaceid;
         if ($extend) {
             $data->extend = get_string('extendevent', 'mod_topomojo');
         }

version.php
@@ -46,7 +46,7 @@ DM24-1175
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2026043018;
+$plugin->version = 2026050701;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2025041400;
@@ -64,4 +64,4 @@ $plugin->dependencies = [
 $plugin->maturity = MATURITY_BETA;
 
 // This is the named version.
-$plugin->release = '1.0.1';
+$plugin->release = '1.0.2';