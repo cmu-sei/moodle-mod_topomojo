1320c1fa287135b22d19f57963385ceefd4764aa
Adam Welle <arwelle@cert.org>
2026-05-12 14:16:26 -0400

    Fix ZIP packaging structure for Moodle installation (#50)
    * Fix ZIP packaging structure for Moodle installation
    
    * Auto-increment version
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- version.php
+++ version.php
@@ -46,7 +46,7 @@ DM24-1175
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2026051200;
+$plugin->version = 2026051201;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2025041400;
